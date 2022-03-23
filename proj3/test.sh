5c04cdcd614de1b00941f6bdf7b1fd9860bb18f7
Sharif Haason <ssh128@scarletmail.rutgers.edu>
2022-03-23 09:27:10 -0400

    Fix sed use

--- proj3/test.sh
+++ proj3/test.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # basic: no username or password posted, no cookies (login)
-curl -v http://localhost:32702
+curl http://localhost:"$1"
 # correct username and password posted, no cookies (success)
-curl -v -d "username=bezos&password=amazon" -c cookies.txt -b cookies.txt http://localhost:32702
+curl -d "username=bezos&password=amazon" -c cookies.txt -b cookies.txt http://localhost:"$1"
 # non-existent username posted with password, no cookies (bad credentials)
-curl -v -d "username=some&password=none" http://localhost:32702
+curl -d "username=some&password=none" http://localhost:"$1"
 # existing username posted with bad password, no cookies (bad credentials)
-curl -v -d "username=bezos&password=none" http://localhost:32702
+curl -d "username=bezos&password=none" http://localhost:"$1"
 # exactly one of username or password posted, other field missing, no cookies (bad credentials)
-curl -v -d "username=bezos" http://localhost:32702
+curl -d "username=bezos" http://localhost:"$1"
 # no username or password posted, valid cookie (success)
-curl -v -c cookies.txt -b cookies.txt http://localhost:32702
+curl -c cookies.txt -b cookies.txt http://localhost:"$1"
 # non-existent username or bad password for existing username, valid cookie (success)
-curl -v -d "username=some&password=none" -c cookies.txt -b cookies.txt http://localhost:32702
+curl -d "username=some&password=none" -c cookies.txt -b cookies.txt http://localhost:"$1"
 # correct username and password, valid cookie (success)
-curl -v -d "username=bezos&password=amazon" -c cookies.txt -b cookies.txt http://localhost:32702
+curl -d "username=bezos&password=amazon" -c cookies.txt -b cookies.txt http://localhost:"$1"
 # correct username and password, invalid cookie (bad credentials)
-sed 's/0/1' cookies.txt
-curl -v -d "username=bezos&password=amazon" -c cookies.txt -b cookies.txt http://localhost:32702
+sed -i 's/0/1/g' cookies.txt
+curl -d "username=bezos&password=amazon" -c cookies.txt -b cookies.txt http://localhost:"$1"
 # logout posted, valid cookie (logout)
-curl -v -d "username=bezos&password=amazon" -c cookies.txt -b cookies.txt http://localhost:32702 > /dev/null
-curl -v -d "action=logout" -c cookies.txt -b cookies.txt http://localhost:32702
+curl -d "username=bezos&password=amazon" -c cookies.txt -b cookies.txt http://localhost:"$1" > /dev/null # regenerate cookie
+curl -d "action=logout" -c cookies.txt -b cookies.txt http://localhost:"$1"
 # logout posted, invalid cookie (logout)
-curl -v -d "username=bezos&password=amazon" -c cookies.txt -b cookies.txt http://localhost:32702 > /dev/null
-sed 's/0/1' cookies.txt
-curl -v -d "action=logout" -c cookies.txt -b cookies.txt http://localhost:32702
+curl -d "username=bezos&password=amazon" -c cookies.txt -b cookies.txt http://localhost:"$1" > /dev/null # regenerate cookie
+sed -i 's/0/1/g' cookies.txt
+curl -d "action=logout" -c cookies.txt -b cookies.txt http://localhost:"$1"
